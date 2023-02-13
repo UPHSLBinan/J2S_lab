f3bad2b002b9e168542f3c76946f8dbe12fc9531
moninaviardo <123358709+Bautista-css@users.noreply.github.com>
2023-02-13 19:05:43 +0800

    Bautista_J2S
    Aasdasd

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-asd
+<html>
+<body>
+
+
+<form action="insert.php" method="post">
+FirstName: <input type="text" name="fname"><br>
+LastName: <input type="text" name="lname"><br>
+Age:  <input type="number" name="age"><br>
+E-mail: <input type="text" name="email"><br>
+Detail: <input type="text" name="detail"><br>
+<input type="submit">
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
