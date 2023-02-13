ffa9c6dfaf9ba37caa5b166425eaab6ba895f680
SinghidRaven <123729292+phillipjanhierparot@users.noreply.github.com>
2023-02-13 18:46:32 +0800

    Change
    change

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-asd
+<html>
+<body>
+
+<form action="index2.php" method="post">
+
+First name: <input type="text" name="Fname"><br>
+Last name: <input type="text" name="Lname"><br>
+Age : <input type="number" name="Age"><br>
+Email: <input type="text" name="Email"><br>
+Detail: <input type="text" name="Detail"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+
+
+
