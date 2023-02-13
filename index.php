64a57847de3a428d1b77b72d9cfa56321d27d9f8
Kevin Patino <124132083+RonCarloGayap@users.noreply.github.com>
2023-02-13 19:02:59 +0800

    Miyake_J2S
    Form Handling

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-asd
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+FName: <input type="text" name="Fname"><br>
+LName: <input type="text" name="Lname"><br>
+Age: <input type="number" name="Age"><br>
+E-mail: <input type="text" name="Email"><br>
+Detail: <input type="text" name="Detail"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,31 @@
+<?php
+
+$servername = "localhost";
+$username = "myadmin";
+$password = "myadmin";
+$dbname = "miyake";
+
+$Fname= $_POST['Fname'];
+$Lname= $_POST['Lname'];
+$Age= $_POST['Age'];
+$Email= $_POST['Email'];
+$Detail= $_POST['Detail'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO users (Fname, Lname, Age, Email, Detail)
+VALUES ('".$Fname."','".$Lname."','".$Age."','".$Email."','".$Detail."') ";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
