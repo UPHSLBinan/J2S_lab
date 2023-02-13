54db2581523f3a71d794f52d2500561e13751889
bdimaguila <123310442+rmndzz@users.noreply.github.com>
2023-02-13 19:06:56 +0800

    ada
    asda

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-asd
+<html>
+<body>
+
+<form action="index.php" method="get">
+First Name: <input type="text" name="name"><br>
+Last Name:  <input type="text" name="name"><br>
+Age:        <input type="number" name="name"><br>
+E-mail:     <input type="email" name="email"><br>D
+Detail:     <input type="text" name="name"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
