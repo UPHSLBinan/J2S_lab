112e5975c3b853cb40ca53fb7002148abdc9e28f
simplybel <patrickjayson900@gmail.com>
2023-02-15 18:12:22 +0800

    test
    test

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-asd
+<html>
+<body>
+
+<form action="save_database.php" method="post">
+First Name: <input type="text" name="fname"><br>
+Last Name: <input type="text" name="lname"><br>
+Age: <input type="number" name="age"><br>
+Email: <input type="email" name="email"><br>
+Detail: <input type="text" name="detail"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
