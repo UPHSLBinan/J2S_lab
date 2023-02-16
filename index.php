08eaceb9e827035684d4509197c653bc0288bc08
JustNeth <123313047+JustNeth@users.noreply.github.com>
2023-02-16 16:43:07 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <html>
-  <body>
-    <form method="POST" action="store_data.php">
-      <label for="FirstName">First Name:</label>
-      <input type="text"  name="FirstName"><br><br>
+<body>
 
-      <label for="LastName">Last Name:</label>
-      <input type="text"  name="LastName"><br><br>
 
-      <label for="Age">Age:</label>
-      <input type="Int"  name="Age"><br><br>
 
-      <label for="Email">Email:</label>
-      <input type="varchar"  name="Email"><br><br>
+<form action="connect.php" method="post">
+Last Name:  <input type="text" name="LastName"><br>
+First Name: <input type="text" name="FirstName"><br>
+Age:        <input type="int"  name="Age"><br>
+Email:      <input type="varchar" name="Email"><br>
+Detail:     <input type="varchar" name="Detail"><br>
+<input type="submit">
+</form>
 
-      <label for="Detail">Detail:</label>
-      <input type="varchar" name="Detail"><br><br>
 
-      <input type="submit" value="Submit">
-    </form>
-  </body>
+
+</body>
 </html>
