4cf8fa819056d075b08cff90496304e8fef32d82
Lanceyy21 <lancecrstbl@gmail.com>
2023-02-27 09:20:38 +0800

    PreAct3_Cristobal

---
+++ PreAct3_ Cristobal/blog/connectdb.php
@@ -0,0 +1,11 @@
+<?php
+
+$con = new mysqli('localhost','lance','lance','userprofile1');
+
+if(!$con) {
+
+    die(mysqli_error($con));
+}
+
+
+?>
---
+++ PreAct3_ Cristobal/blog/user.php
@@ -0,0 +1,93 @@
+<?php
+include 'connectdb.php';
+
+if(isset($_POST['submit'])) {
+    $username=$_POST['username']; 
+    $password=$_POST['password']; 
+    $firstname=$_POST['firstname']; 
+    $lastname=$_POST['lastname']; 
+    $age=$_POST['age']; 
+    $birthday=$_POST['birthday']; 
+    $address=$_POST['address']; 
+
+
+    $sql = "insert into `data` (username,password,firstname,lastname,age,birthday,address)
+    values('$username','$password','$firstname','$lastname','$age','$birthday','$address')";
+
+    $result=mysqli_query($con,$sql);
+    if($result){
+       // echo "Data Inserted Successfully";
+       header('location:display.php');
+        }else{
+            die(mysqli_error($con));
+        }
+}
+
+
+?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+   
+    <span class="border border-light"></span>
+
+    <title>User Profile</title> 
+  </head>
+  <body>
+  <h1>Sign Up</h1>
+ 
+   <div class="container-fluid my-5">
+
+   <form method = "post">
+
+   <div class="form-floating">
+    <label for="floatingInput" >User Name</label>
+    <input type="text" class="form-control" id="floatingInput" placeholder="Please enter username" name="username" autocomplete = "off">
+</div>
+<div class="form-floating">
+    <label for="floatingInput" >Password</label>
+    <input type="text" class="form-control" id="floatingInput" placeholder="enter password" name="password" autocomplete = "off">
+</div>
+  <div class="form-floating">
+    <label for="floatingInput" >First Name</label>
+    <input type="text" class="form-control" id="floatingInput" placeholder="First Name" name="firstname" autocomplete = "off">
+</div>
+
+<div class="form-group">
+    <label >Last Name</label>
+    <input type="text" class="form-control"  placeholder="Last Name" name="lastname" autocomplete = "off">
+</div>
+
+<div class="form-group">
+    <label >Age</label>
+    <input type="text" class="form-control"  placeholder="Enter Your Age" name="age" autocomplete = "off">
+</div>
+
+<div class="form-group">
+    <label >Birthday</label>
+    <input type="text" class="form-control"  placeholder="Enter Your Birthday" name="birthday" autocomplete = "off">
+</div>
+
+<div class="form-group">
+    <label >Address</label>
+    <input type="text" class="form-control"  placeholder="Enter Your Address" name="address" autocomplete = "off">
+</div>
+    
+  <button type="submit" class="btn btn-outline-primary" name = "submit">Submit</button>
+</form>
+   </div>
+
+    
+    
+  </body>
+</html> 
