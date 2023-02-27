02d8af1ea655bab05cfa9f02cf009643e67d23e4
TroyCualquiera <123392294+TroyCualquiera@users.noreply.github.com>
2023-02-28 02:26:03 +0800

    Pogi Si Troy
    Hahahahha

---
+++ PreAct3Cualquiera/blog.php
@@ -0,0 +1,64 @@
+<?php
+include 'connection.php'; ?>
+<!DOCTYPE html>
+	<html lang="en">
+<head>
+	<title>Blog Post</title>
+</head>
+<body>
+
+<body style="background-color: #FEE5C8">
+	<h1 style="background-color: #3366CC; color: #FFFFFF"><center>Exploring the Exciting World of Valorant</center></h1>
+	<h2 style="color: #333333"><center>Troy Cualquiera</center></h2>
+	<h3 style="color: #333333"><center>Feb 27, 2023</center></h3>
+	<ul type="disc">
+<br>
+		
+<h1> Valorant </h1>
+			
+<b>
+Are you looking for an intense first-person shooter game that offers a unique gaming experience? Look no further than Valorant, Riot Games' multiplayer tactical shooter that has taken the gaming world by storm.</p></li>
+
+		
+
+			
+
+At its core, Valorant is a game of strategy and teamwork. Players are split into two teams of five and are tasked with either attacking or defending a set of objectives. Along the way, players can purchase weapons and abilities using in-game currency earned by performing well in matches.</p></li>
+
+		
+
+	<h1> Agents </h1>
+
+One of the most interesting aspects of Valorant is the inclusion of Agents, each with their own set of unique abilities that can be used to gain an advantage over the other team. From healing powers to teleports, these abilities add a layer of strategy to the game that sets it apart from other first-person shooters.</p></li>
+
+		
+
+		<h1> Strategy</h1>	
+
+Valorant also boasts a high level of technical gameplay, with a heavy focus on precision and accuracy. This is particularly evident in the game's gun mechanics, which require players to master recoil and bullet spread in order to be successful. </p></li>
+
+		
+
+		<h1> Confidence</h1>	
+
+		
+
+			
+But don't let the technical gameplay intimidate you. With its vibrant graphics and fast-paced action, Valorant is a game that's easy to pick up but difficult to master. And with a rapidly growing community of players and esports teams, there's never been a better time to jump in and start playing. </p></li>
+
+		
+<h1> Game</h1>
+			
+
+Whether you're a seasoned gamer looking for a new challenge or a casual player looking to try something new, Valorant has something for everyone. So what are you waiting for? Grab your keyboard and mouse and join the fight in this exciting and immersive world of tactical first-person shooting.<p></li>
+
+	</b>	
+
+
+
+		
+
+</li>
+
+</body>
+</html>
---
+++ PreAct3Cualquiera/edit.php
@@ -0,0 +1,101 @@
+<?php
+include 'connection.php';
+$id=$_GET['editid'];
+$sql="Select *from `troy` where id=$id";
+$result=mysqli_query($con,$sql);
+$row=mysqli_fetch_assoc($result);
+
+$username=$row['username'];
+$password=$row['password'];
+$firstname=$row['firstname'];
+$lastname=$row['lastname'];
+$age=$row['age'];
+$birthday=$row['birthday'];
+$address=$row['address'];
+
+if(isset($_POST['submit'])) {
+    $username=$_POST['username']; 
+    $password=$_POST['password']; 
+    $firstname=$_POST['firstname']; 
+    $lastname=$_POST['lastname']; 
+    $age=$_POST['age']; 
+    $birthday=$_POST['birthday']; 
+    $address=$_POST['address']; 
+
+
+    $sql = "update `data` set id=$id, username='$username',firstname='$password',firstname='$firstname',lastname='$lastname',age='$age',birthday='$birthday',address='$address'
+    where id=$id";
+    $result=mysqli_query($con,$sql);
+    if($result){
+       //echo "Edit Successful";
+       header('location:display.php');
+        }else{
+            die(mysqli_error($con));
+        }
+}
+
+?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+
+    <title>User Profile</title> 
+  </head>
+  <body>
+   <div class="container my-5">
+
+   <form method = "post">
+   <div class="form-group">
+    <label >Username</label>
+    <input type="text" class="form-control"  placeholder="Please enter new username" name="username" autocomplete = "off" value =<?php echo $username; ?>>
+</div>
+<div class="form-group">
+    <label >Password</label>
+    <input type="text" class="form-control"  placeholder="Please enter new password" name="password" autocomplete = "off" value =<?php echo $password; ?>>
+</div>
+  
+  
+  
+   <div class="form-group">
+    <label >First Name</label>
+    <input type="text" class="form-control"  placeholder="First Name" name="firstname" autocomplete = "off" value =<?php echo $firstname; ?>>
+</div>
+
+<div class="form-group">
+    <label >Last Name</label>
+    <input type="text" class="form-control"  placeholder="Last Name" name="lastname" autocomplete = "off"value = <?php echo $lastname;?>>
+</div>
+
+<div class="form-group">
+    <label >Age</label>
+    <input type="text" class="form-control"  placeholder="Enter Your Age" name="age" autocomplete = "off"value = <?php echo $age;?>>
+</div>
+
+<div class="form-group">
+    <label >Birthday</label>
+    <input type="text" class="form-control"  placeholder="Enter Your Birthday" name="birthday" autocomplete = "off"value = <?php echo $birthday;?>>
+</div>
+
+<div class="form-group">
+    <label >Address</label>
+    <input type="text" class="form-control"  placeholder="Enter Your Address" name="address" autocomplete = "off"value = <?php echo $address;?>>
+</div>
+    
+  <button type="submit" class="btn btn-primary" name = "submit">Edit</button>
+</form>
+   </div>
+
+    
+    
+  </body>
+</html> 
